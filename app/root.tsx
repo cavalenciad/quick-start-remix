0ed4d52e19fc6c91343261324c162abca8f3f604
cavalenciad <cavalenciad@outlook.com>
2025-02-09 03:09:54 -0500

    Update: Search field is available and functional

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,7 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { 
+  LinksFunction,
+  LoaderFunctionArgs,
+} from "@remix-run/node";
 import appStylesHref from "./app.css?url";
 import { json, redirect } from "@remix-run/node";
 import {
@@ -19,8 +22,12 @@ export const action = async () => {
   return redirect(`/contacts/${contact.id}/edit`);
 };
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
